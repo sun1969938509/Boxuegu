3cb747a36e27c3ff1895307d730934ac80306ffa
sun1969938509 <1969938509@qq.com>
2018-03-27 10:26:51 +0800

    敲到设置密保和找回密码那里了

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SettingActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SettingActivity.java
@@ -1,15 +1,84 @@
 package cn.edu.gdmec.android.boxuegu.activity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
 
 import cn.edu.gdmec.android.boxuegu.R;
 
 public class SettingActivity extends AppCompatActivity {
-
+     private TextView tv_main_title;
+     private TextView tv_back;
+     private RelativeLayout rl_title_bar;
+     private RelativeLayout rl_modify_psw,rl_security_setting,rl_exit_login;
+     public static SettingActivity instance=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        //设置此界面为界面
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        instance=this;
+        init();
     }
+    private void init(){
+        tv_main_title=(TextView)findViewById(R.id.tv_main_title);
+        tv_main_title.setText("设置");
+        tv_back=(TextView)findViewById(R.id.tv_back);
+        rl_title_bar=(RelativeLayout)findViewById(R.id.title_bar);
+        rl_title_bar.setBackgroundColor(Color.parseColor("#30B4FF"));
+        rl_modify_psw=(RelativeLayout)findViewById(R.id.rl_modify_psw);
+        rl_security_setting=(RelativeLayout)findViewById(R.id.rl_security_setting);
+        rl_exit_login=(RelativeLayout)findViewById(R.id.rl_exit_login);
+        tv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SettingActivity.this.finish();
+            }
+        });
+        rl_modify_psw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到修改密码的界面
+                Intent intent=new Intent(SettingActivity.this,ModifyPswActivity.class);
+                startActivity(intent);
+            }
+        });
+        rl_security_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到设置密保界面
+            }
+        });
+        rl_exit_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SettingActivity.this,"退出登录成功",Toast.LENGTH_SHORT).show();
+                clearLoginStatus();
+                Intent data=new Intent();
+                data.putExtra("isLogin",false);
+                setResult(RESULT_OK,data);
+                SettingActivity.this.finish();
+            }
+        });
+    }
+    private void clearLoginStatus(){
+        SharedPreferences sp=getSharedPreferences("loginInfo",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=sp.edit();
+        editor.putBoolean("isLogin",false);
+        editor.putString("loginUserName","");
+        editor.commit();
+    }
+
+
 }
--- app/src/main/java/cn/edu/gdmec/android/boxuegu/view/MyInfoView.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/view/MyInfoView.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import cn.edu.gdmec.android.boxuegu.R;
 import cn.edu.gdmec.android.boxuegu.activity.LoginActivity;
+import cn.edu.gdmec.android.boxuegu.activity.SettingActivity;
 import cn.edu.gdmec.android.boxuegu.utils.AnalysisUtils;
 
 /**
@@ -71,6 +72,8 @@ public class MyInfoView {
             public void onClick(View v) {
                 if(readLoginStatus()){
                     //跳转到设置界面
+                    Intent intent=new Intent(mContext, SettingActivity.class);
+                    mContext.startActivityForResult(intent,1);
                 }else {
                     Toast.makeText(mContext,"您还没登录，请先登录",Toast.LENGTH_SHORT).show();
                 }
